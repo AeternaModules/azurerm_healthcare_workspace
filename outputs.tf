d466b3a17db4f55e226ae4e46bcd9f3536cd1535
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "healthcare_workspaces" {
   description = "All healthcare_workspace resources"
   value       = azurerm_healthcare_workspace.healthcare_workspaces
 }
+output "healthcare_workspaces_location" {
+  description = "List of location values across all healthcare_workspaces"
+  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.location]
+}
+output "healthcare_workspaces_name" {
+  description = "List of name values across all healthcare_workspaces"
+  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.name]
+}
+output "healthcare_workspaces_private_endpoint_connection" {
+  description = "List of private_endpoint_connection values across all healthcare_workspaces"
+  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.private_endpoint_connection]
+}
+output "healthcare_workspaces_resource_group_name" {
+  description = "List of resource_group_name values across all healthcare_workspaces"
+  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.resource_group_name]
+}
+output "healthcare_workspaces_tags" {
+  description = "List of tags values across all healthcare_workspaces"
+  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.tags]
+}
 
