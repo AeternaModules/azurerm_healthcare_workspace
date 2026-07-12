8ab3357d06fefcef86a19cedf18385980f6110ac
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "healthcare_workspaces" {
-  description = "All healthcare_workspace resources"
-  value       = azurerm_healthcare_workspace.healthcare_workspaces
-}
 output "healthcare_workspaces_location" {
-  description = "List of location values across all healthcare_workspaces"
-  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.location]
+  description = "Map of location values across all healthcare_workspaces, keyed the same as var.healthcare_workspaces"
+  value       = { for k, v in azurerm_healthcare_workspace.healthcare_workspaces : k => v.location }
 }
 output "healthcare_workspaces_name" {
-  description = "List of name values across all healthcare_workspaces"
-  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.name]
+  description = "Map of name values across all healthcare_workspaces, keyed the same as var.healthcare_workspaces"
+  value       = { for k, v in azurerm_healthcare_workspace.healthcare_workspaces : k => v.name }
 }
 output "healthcare_workspaces_private_endpoint_connection" {
-  description = "List of private_endpoint_connection values across all healthcare_workspaces"
-  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.private_endpoint_connection]
+  description = "Map of private_endpoint_connection values across all healthcare_workspaces, keyed the same as var.healthcare_workspaces"
+  value       = { for k, v in azurerm_healthcare_workspace.healthcare_workspaces : k => v.private_endpoint_connection }
 }
 output "healthcare_workspaces_resource_group_name" {
-  description = "List of resource_group_name values across all healthcare_workspaces"
-  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all healthcare_workspaces, keyed the same as var.healthcare_workspaces"
+  value       = { for k, v in azurerm_healthcare_workspace.healthcare_workspaces : k => v.resource_group_name }
 }
 output "healthcare_workspaces_tags" {
-  description = "List of tags values across all healthcare_workspaces"
-  value       = [for k, v in azurerm_healthcare_workspace.healthcare_workspaces : v.tags]
+  description = "Map of tags values across all healthcare_workspaces, keyed the same as var.healthcare_workspaces"
+  value       = { for k, v in azurerm_healthcare_workspace.healthcare_workspaces : k => v.tags }
 }
 
